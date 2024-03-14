a6eaa5d85b336d74a5900d60d4aa577d15759b06
cco4z8 <151125814+cco4z8@users.noreply.github.com>
2024-03-14 14:52:18 +0800

    Add files via upload

--- ex3/main.c
+++ ex3/main.c
@@ -6,79 +6,55 @@ Welcome to GDB Online.
   Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-typedef struct employee
-{
-    int id;
-    char name[64];
-    int age;
-    float salary;
-}employee_t;
+typedef struct employee {
+    int em_id;
+    char em_name[10];
+    int em_age;
+    char em_phone[12];
+    float em_salary;
+} employee_t;
 
-void emp_info(employee_t emp){
-    printf("employee id:%d\n",emp.id);
-    printf("employee name:%s\n",emp.name);
-    printf("employee age:%d\n",emp.age);
-    printf("employee salary:%02f\n\n",emp.salary);
-}
-int emp_average_age(employee_t emp[],int n){
-    int sum;
-    for(int i=0;i<n;i++){
-        sum+=emp[i].age;
-    }
-    return sum/n;
+void employee_info(employee_t my_emp) {
+    printf("%d\n", my_emp.em_id);
+    printf("%s\n", my_emp.em_name);
+    printf("%d\n", my_emp.em_age);
+    printf("%s\n", my_emp.em_phone);
+    printf("%f\n", my_emp.em_salary);
 }
 
-void emp_writefile(employee_t emp[], int n){
-    FILE *fp = fopen("employee.bin","wb+");
+int main() {
+    employee_t my_emp;
 
-    for(int i=0;i<n;i++){
-        fwrite(&emp[i],sizeof(employee_t),1,fp);
+    FILE *file_out = fopen("employee.bin", "wb");
+    if (file_out == NULL) {
+        perror("Error opening file");
+        return 1;
     }
-    fclose(fp);
-    return ;
-}
-void emp_readfile(employee_t emp[]){
-    FILE *fp=fopen("employee.bin","rb");
-    int i=0;
-    employee_t tmp;
-    while(fread(&tmp,sizeof(employee_t),1,fp)){
-        emp[i].id=tmp.id;
-        emp[i].age=tmp.age;
-        emp[i].salary=tmp.salary;
-        strcpy(emp[i].name,tmp.name);
-        printf("[%d] %d %s\n",i,emp[i].id,emp[i].name);
-        i++;
+
+    strcpy(my_emp.em_name, "IU Lee");
+    strcpy(my_emp.em_phone, "0937123456");
+    my_emp.em_id = 100;
+    my_emp.em_age = 18;
+    my_emp.em_salary = 1000.30;
+
+    fwrite(&my_emp, sizeof(employee_t), 1, file_out);
+
+    fclose(file_out);
+
+    FILE *file_in = fopen("employee.bin", "rb");
+    if (file_in == NULL) {
+        perror("Error opening file");
+        return 1;
     }
-    fclose(fp);
-    return;
-}
-int main(){
 
-    employee_t emp[3];
+    fread(&my_emp, sizeof(employee_t), 1, file_in);
 
-    emp[0].id=1;
-    emp[0].age=20;
-    emp[0].salary=30000.0;
-    strcpy(emp[0].name,"IU");
+    fclose(file_in);
 
-    emp[1].id=2;
-    emp[1].age=26;
-    emp[1].salary=36000.0;
-    strcpy(emp[1].name,"taylor");
+    employee_info(my_emp);
 
-    emp[2].id=3;
-    emp[2].age=31;
-    emp[2].salary=90000.0;
-    strcpy(emp[2].name,"swift");
-
-    emp_writefile(emp,3);
-    employee_t read_emp[10];
-    emp_readfile(read_emp);
-
-    for(int i=0;i<3;i++){
-        emp_info(read_emp[i]);
-    }
+    return 0;
 }
